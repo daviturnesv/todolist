673dc246158c2105c45a3b27c32bb1c93d87d3db
daviturnesv <davi.turnesv@gmail.com>
2025-01-27 19:46:39 -0300

    Password hash & Tasks implementation

---
+++ src/main/java/br/com/daviturnesv/todolist/task/TaskModel.java
@@ -0,0 +1,49 @@
+package br.com.daviturnesv.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+    /**
+     * Caracteristicas de um Tarefa:
+     * 
+     * ID
+     * Usuario (ID_USUARIO)
+     * Descriçao
+     * Titulo
+     * Data de inicio
+     * Data de termino
+     * Prioridade
+     * 
+     */
+
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String description;
+
+    @Column(length = 40)
+    private String title;
+    private LocalDateTime startsAt;
+    private LocalDateTime endsAt;
+    private String priority;
+
+    private UUID idUser;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    
+
+}
--- src/main/java/br/com/daviturnesv/todolist/user/UserController.java
+++ src/main/java/br/com/daviturnesv/todolist/user/UserController.java
@@ -3,6 +3,13 @@ package br.com.daviturnesv.todolist.user;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import io.micrometer.core.ipc.http.HttpSender.Response;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -10,8 +17,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario ja existe");
+        }
+
+        var senhaCriptografada = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(senhaCriptografada);
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
